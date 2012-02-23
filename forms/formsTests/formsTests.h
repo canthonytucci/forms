f95ffaac93ae59bae513cca4fbc3ba40ca195375
Chris Tucci <canthonytucci@gmail.com>
2012-02-23 18:31:26 -0500

    Set Up Xcode Project

---
+++ forms/formsTests/formsTests.h
@@ -0,0 +1,13 @@
+//
+//  formsTests.h
+//  formsTests
+//
+//  Created by Chris Tucci on 2/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface formsTests : SenTestCase
+
+@end
---
+++ forms/formsTests/formsTests.m
@@ -0,0 +1,32 @@
+//
+//  formsTests.m
+//  formsTests
+//
+//  Created by Chris Tucci on 2/23/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "formsTests.h"
+
+@implementation formsTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in formsTests");
+}
+
+@end
